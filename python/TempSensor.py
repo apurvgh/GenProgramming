484b629ecad830bf46d8730bf7f30e055db28c56
Apurv <Apurv@surface-genprogram>
2018-01-14 16:23:02 -0600

    Adding python scripts

---
+++ python/TempSensor.py
@@ -0,0 +1,67 @@
+#This script uses AdaFurit Library to measure temp
+#!/usr/bin/python
+
+import sys
+import Adafruit_DHT
+import math
+import json
+import requests
+
+
+
+def get_frost_point_c(t_air_c, dew_point_c):
+    """Compute the frost point in degrees Celsius
+    :param t_air_c: current ambient temperature in degrees Celsius
+    :type t_air_c: float
+    :param dew_point_c: current dew point in degrees Celsius
+    :type dew_point_c: float
+    :return: the frost point in degrees Celsius
+    :rtype: float
+    """
+    dew_point_k = 273.15 + dew_point_c
+    t_air_k = 273.15 + t_air_c
+    frost_point_k = dew_point_k - t_air_k + 2671.02 / ((2954.61 / t_air_k) + 2.193665 * math.log(t_air_k) - 13.3448)
+    return frost_point_k - 273.15
+
+
+def get_dew_point_c(t_air_c, rel_humidity):
+    """Compute the dew point in degrees Celsius
+    :param t_air_c: current ambient temperature in degrees Celsius
+    :type t_air_c: float
+    :param rel_humidity: relative humidity in %
+    :type rel_humidity: float
+    :return: the dew point in degrees Celsius
+    :rtype: float
+    """
+    A = 17.27
+    B = 237.7
+    alpha = ((A * t_air_c) / (B + t_air_c)) + math.log(rel_humidity/100.0)
+    return (B * alpha) / (A - alpha)
+
+#set up variables
+CLIENT_ID = "p-jcoLKBynTLew"
+CLIENT_SECRET = "gko_LXELoV07ZBNUXrvWZfzE3aI"
+REDIRECT_URI = "http://localhost:65010/reddit_callback"
+
+
+#send the data to Dynamics CRM Server
+api_token = 'your_api_token'
+api_url_base = 'https://api.digitalocean.com/v2/'
+headers = {'Content-Type': 'application/json',
+           'Authorization': 'Bearer {0}'.format(api_token)}
+
+while True:
+
+    rh, t = Adafruit_DHT.read_retry(11, 4)
+    #temperature = temperature * 9/5.0 + 32
+    dw = get_dew_point_c(t,rh)
+    fp= get_frost_point_c(t,dw)
+    print 'TEMP (c): {0:0.1f} C HUMIDITY: {1:0.1f} % DEW POINT: {2:0.1f} FROST POINT {3:0.1f}'.format(t,rh,dw,fp)
+
+
+
+
+
+
+
+
